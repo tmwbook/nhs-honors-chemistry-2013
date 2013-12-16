9512ffdb526adeea11bc8dadc5c5171a59751783
Thomas White <tmwbook@gmail.com>
2013-12-15 21:34:22 -0500

    Consolidated files due to it being a bad idea in the first place, now all elements will be stored in 3 files, with the search index the Table.py.  Not sure how this will effect the eventual move to centralization for all the processes.

--- Periodic Table/NonMetal.py
+++ Periodic Table/NonMetal.py
@@ -4,3 +4,47 @@ Created on Dec 6, 2013
 @author: thomas
 '''
 PROPERTIES = ['non malleable', 'non ductile', 'insulator', 'non elasticity']
+
+from NonMetal import NonMetal
+
+class Halogen(NonMetal):
+
+    CHARGE = 3
+
+    def __init__(self, atomicNumber, atomicMass):
+        self.atomicNumber = atomicNumber
+        self.atomicMass = atomicMass
+
+fluorine = Halogen(9, 18.998403)
+chlorine = Halogen(17, 35.453)
+bromine = Halogen(35, 79.904)
+iodine = Halogen(53, 126.905)
+astatine = Halogen(85, 210)
+
+class Chalocogen(NonMetal):
+
+    CHARGE = 3
+
+    def __init__(self, atomicNumber, atomicMass):
+        self.atomicNumber = atomicNumber
+        self.atomicMass = atomicMass
+
+oxygen = Chalocogen(8, 15.9994)
+sulfur = Chalocogen(16, 31.06)
+selenium = Chalocogen(34, 78.96)
+tellurium = Chalocogen(52, 127.6)
+polonium = Chalocogen(84, 209)
+
+class Pnictogen(NonMetal):
+
+    CHARGE = 3
+
+    def __init__(self, atomicNumber, atomicMass):
+        self.atomicNumber = atomicNumber
+        self.atomicMass = atomicMass
+
+nitrogen = Pnictogen(7, 14.0067)
+phosphorus = Pnictogen(15, 30.97376)
+arsenic = Pnictogen(33, 74.9216)
+antimony = Pnictogen(51, 121.75)
+bismuth = Pnictogen(83, 209)
--- Periodic Table/Table.py
+++ Periodic Table/Table.py
@@ -4,7 +4,12 @@ Created on Dec 6, 2013
 @author: thomas
 '''
 
+import Metal
+import NonMetal
+
 NUMBER_OF_ELEMENTS = 118
 
 class Table(object):
     NUMBER_OF_ELEMENTS = 118 #something to stop an error
+
+elementLocations = {} #Add locations of Elements in the Metal and NonMetal files
