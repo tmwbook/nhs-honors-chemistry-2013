eaa528b4ee2dfa0d804f5020837c296216d9f84d
Andrew <Andrew19981998@hotmail.com>
2014-03-29 20:24:31 -0700

    Transition metals

--- Python/Elements/TransitionMetal.py
+++ Python/Elements/TransitionMetal.py
@@ -9,7 +9,48 @@ class TransitionMetal():
 
     PROPERTIES = PROPERTIES
 
-    def __init__(self, atomicNumber, atomicMass, charge):
+    def __init__(self, atomicNumber, atomicMass):
         self.atomicNumber = atomicNumber
         self.atomicMass = atomicMass
-        self.charge = charge
+
+scandium = TransitionMetal(21, 44.955)
+titanium = TransitionMetal(22, 47.867)
+vanadium = TransitionMetal(23, 50.9415)
+chromium = TransitionMetal(24, 51.9961)
+manganese = TransitionMetal(25, 54.938)
+iron = TransitionMetal(26, 55.845)
+cobalt = TransitionMetal(27, 58,933)
+nickel = TransitionMetal(28, 58.6934)
+copper = TransitionMetal(29, 63.546)
+zinc = TransitionMetal(30, 65.38)
+
+yttrium = TransitionMetal(39, 88.90585)
+zirconium = TransitionMetal(40, 91.224)
+niobium = TransitionMetal(41, 92.90638)
+molybdenum = TransitionMetal(42, 95.96)
+technetium = TransitionMetal(43, 98)
+ruthenium = TransitionMetal(44, 101.07)
+rhodium = TransitionMetal(45, 102.90)
+palladium = TransitionMetal(46, 106.42)
+silver = TransitionMetal(47, 107.8682)
+cadmium = TransitionMetal(48, 112.411)
+
+hafnium = TransitionMetal(72, 178,49)
+tantalum = TransitionMetal(73, 180.94)
+tungsten = TransitionMetal(74, 183.84)
+rhenium = TransitionMetal(75, )
+osmium = TransitionMetal(76)
+iridium = TransitionMetal(77)
+platinum = TransitionMetal(78)
+gold = TransitionMetal(79)
+mercury = TransitionMetal(80)
+
+rutherfordium = TransitionMetal(104)
+dubnium  = TransitionMetal(105)
+seaborgium = TransitionMetal(106)
+bohrium = TransitionMetal(107)
+hassium = TransitionMetal(108)
+meitnerium = TransitionMetal(109)
+darmstadtium = TransitionMetal(110)
+roentgenium = TransitionMetal(111)
+copernicium = TransitionMetal(112)
