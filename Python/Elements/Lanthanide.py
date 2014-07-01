1c49f2678effafea1ed3b30ca821bc92dd8130eb
thomas <tmwbook>
2014-07-01 18:35:55 -0400

    Added electron configuration to lanthanide series

--- Python/Elements/Lanthanide.py
+++ Python/Elements/Lanthanide.py
@@ -7,26 +7,26 @@ Created on April 2, 2014
 
 class Lanthanide(object):
 
-
-    def __init__(self, atomicNumber, atomicMass):
+    def __init__(self, atomicNumber, atomicMass, lastElectConf):
         self.atomicNumber = atomicNumber
         self.atomicMass = atomicMass
+        self.lastElectConf = lastElectConf
 
 
-lanthanum = Lanthanide(57, 138.9)
-cerium = Lanthanide(58, 140.116)
-praseodymium = Lanthanide(59, 140.9)
-neodymium = Lanthanide(60, 144.242)
-promethium = Lanthanide(61, 145)
-samarium = Lanthanide(62, 150.36)
+lanthanum = Lanthanide(57, 138.9, [" 5d^", "1"])
+cerium = Lanthanide(58, 140.116, [" 4f^", "1"])
+praseodymium = Lanthanide(59, 140.9, [" 4f^", "2"])
+neodymium = Lanthanide(60, 144.242, [" 4f^", "3"])
+promethium = Lanthanide(61, 145, [" 4f^", "4"])
+samarium = Lanthanide(62, 150.36, [" 4f^", "5"])
 
-europium = Lanthanide(63, 151.964)
-gadolinium = Lanthanide(64, 157.25)
-terbium = Lanthanide(65, 158.92)
-dysprosium = Lanthanide(66, 162.5)
-holmium = Lanthanide(67, 164.93)
-erbium = Lanthanide(68, 167.259)
+europium = Lanthanide(63, 151.964, [" 4f^", "6"])
+gadolinium = Lanthanide(64, 157.25, [" 4f^", "7"])
+terbium = Lanthanide(65, 158.92, [" 4f^", "8"])
+dysprosium = Lanthanide(66, 162.5, [" 4f^", "9"])
+holmium = Lanthanide(67, 164.93, [" 4f^", "10"])
+erbium = Lanthanide(68, 167.259, [" 4f^", "11"])
 
-thulium = Lanthanide(69, 168.93)
-ytterbium = Lanthanide(70, 173.054)
-lutetium = Lanthanide(71, 174.6338)
+thulium = Lanthanide(69, 168.93, [" 4f^", "12"])
+ytterbium = Lanthanide(70, 173.054, [" 4f^", "13"])
+lutetium = Lanthanide(71, 174.6338, ["4f^", "14"])
